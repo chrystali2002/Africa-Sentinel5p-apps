6c4ae39c4f645e0b7a69ec4695a97f55d7711b8c
John Salako <salakojo@msu.edu>
2024-02-27 18:48:52 -0500

    updated page icon

--- app_co2.py
+++ app_co2.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 
-# setting webpage title and emoji
-st.set_page_config(page_title='CO2 emission Monitoring', page_icon=':tada:', layout='wide')
+# setting webpage title and icon
+st.set_page_config(page_title="Africa's CO2 emission Monitoring", page_icon='🛰️', layout='wide')
 
 # temporal header
 st.subheader('Live Monitoring of CO2 in Africa')

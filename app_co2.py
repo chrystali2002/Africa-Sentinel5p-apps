520066c7f7e6e11376f51b3dc1ce4ea3f40879c6
John Salako <salakojo@msu.edu>
2024-02-27 14:48:23 -0500

    Add co2 Monitoring

---
+++ app_co2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+# setting webpage title and emoji
+st.set_page_config(page_title='CO2 emission Monitoring', page_icon=':tada:', layout='wide')
+
+# temporal header
+st.subheader('Live Monitoring of CO2 in Africa')

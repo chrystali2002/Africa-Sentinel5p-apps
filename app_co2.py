26194f62a760cc7dc249cff46c6ff7a3747dc90d
John Salako <salakojo@msu.edu>
2024-02-28 17:53:54 -0500

    updated scripts

--- app_co2.py
+++ app_co2.py
@@ -1,13 +1,5 @@
-from pdb import line_prefix
 import streamlit as st
 import geemap
-#from streamlit_metrics import metric, metric_row
-from streamlit_ace import st_ace
-import pandas as pd
-import leafmap.foliumap as leafmap
-import ee
-import extra_streamlit_components as stx
-import re
 
 # setting webpage title and icon
 st.set_page_config(page_title="Africa's CO2 emission Monitoring", page_icon='🛰️', layout='wide')

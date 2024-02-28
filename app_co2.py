fc9114bb72cdc43be0e9add128aa2c7a252e0af0
John Salako <salakojo@msu.edu>
2024-02-28 17:28:54 -0500

    app update

--- app_co2.py
+++ app_co2.py
@@ -4,6 +4,7 @@ import geemap
 #from streamlit_metrics import metric, metric_row
 from streamlit_ace import st_ace
 import pandas as pd
+import leafmap.foliumap as leafmap
 import ee
 import extra_streamlit_components as stx
 import re
@@ -15,4 +16,12 @@ st.set_page_config(page_title="Africa's CO2 emission Monitoring", page_icon='ðŸ›
 st.subheader('Live Monitoring of CO2 in Africa')
 
 
+with st.expander("See source code"):
+    with st.echo():
+        m = leafmap.Map()
+        m.split_map(
+            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
+        )
+        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
 
+m.to_streamlit(height=700)

75ff909c62ba972f9649a787d974fe1afefdce28
John Salako <salakojo@msu.edu>
2024-03-01 10:51:49 -0500

    app update

--- app_co2.py
+++ app_co2.py
@@ -30,6 +30,7 @@ m.add_basemap("SATELLITE")
 # getting Africa shapefile
 shp_path = 'Africa_shp/Africa_Boundaries.shp'
 africa = geemap.shp_to_ee(shp_path)
+africa_json = 'africa_outline.geojson'
 
 
 collection = ee.ImageCollection('COPERNICUS/S5P/NRTI/L3_CO')\
@@ -43,7 +44,7 @@ band_viz = {
 }
 
 m.addLayer(collection.mean(), band_viz, 'S5P CO')
-m.add_shapefile(shp_path,'Africa')
+m.add_geojson(africa_json,'Africa')
 # m.add_colorbar(band_viz, label='CO concentrations', layer_name='Colorbar',position='bottomright',
 #                background_color='white',vmin=0, vmax=0.5)
 m.add_colormap(vis_params=band_viz, label='CO concentrations',

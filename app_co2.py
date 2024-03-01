c674881bbe98b012bda0c49491d30af861284354
John Salako <salakojo@msu.edu>
2024-03-01 04:45:39 -0500

    app update

--- app_co2.py
+++ app_co2.py
@@ -1,8 +1,7 @@
 import ee
 import json
 import streamlit as st
-# import geemap.foliumap as geemap
-import geemap
+import geemap.foliumap as geemap
 
 # setting webpage title and icon
 st.set_page_config(page_title="Africa's CO2 emission Monitoring", page_icon='🛰️', layout='wide')
@@ -21,8 +20,8 @@ ee.Initialize(credentials)
 
 
 m = geemap.Map(center=[-2.635789, 24.433594], zoom=3)
-#m.add_basemap("OpenTopoMap")
-#m.add_basemap("SATELLITE")
+m.add_basemap("OpenTopoMap")
+m.add_basemap("SATELLITE")
 
 
 collection = ee.ImageCollection('COPERNICUS/S5P/NRTI/L3_CO')\
@@ -37,6 +36,6 @@ band_viz = {
 
 #palette: ['black', 'blue', 'purple', 'cyan', 'green', 'yellow', 'red']
 m.addLayer(collection.mean(), band_viz, 'S5P CO')
-# m.add_colormap(vis_params=band_viz, label='CO concentrations', layer_name='Colorbar',position=(0,0),
-#               vmin=0, vmax=0.5)
+m.add_colorbar(band_viz, label='CO concentrations', layer_name='Colorbar',position='bottomright',
+               background_color='white',vmin=0, vmax=0.5)
 m.to_streamlit(height=500)

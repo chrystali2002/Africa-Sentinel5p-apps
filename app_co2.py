60095f22dceb165a74bc2b5d86042357676e795e
John Salako <salakojo@msu.edu>
2024-03-01 20:11:24 -0500

    app update

--- app_co2.py
+++ app_co2.py
@@ -71,5 +71,5 @@ m.addLayer(africa_col, band_viz, 'S5P CO')
 #                background_color='white',vmin=0, vmax=0.5)
 m.add_colormap(vis_params=band_viz, label='CO concentrations',
                width=2.5, height=0.2, vmax=0.05, vmin=0.0)
-m.addLayer(study_feature, {}, 'Africa')
+#m.addLayer(study_feature, {}, 'Africa')
 m.to_streamlit(height=600, width=700)
